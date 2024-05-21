0a7e973f6b380bddb444e002a915038274ccb962
Shyam Bharati <sbharati@visteon.com>
2024-05-21 15:25:26 +0530

    more cpp topic covered

--- CppAdvance.cpp
+++ CppAdvance.cpp
@@ -23,20 +23,139 @@
 
 
 #include <iostream>
+//#include "adder.h"
+#define END return 0
+#define ENDMESSAGE cout << "Program ends here\n"
+#define EXINT int32_t
+#define console_log(a) cout << a << endl
+#define console_log2(a,b) cout << a << b << endl
+
 using namespace std;
 
 int memoryExample();
+int headerFileExample();
+int pointerAdvance();
+int nullExample();
+int veriadicExample();
+EXINT factorial(EXINT a);
 
 struct User {
     const int id;
     int age;
-    User() :id(100), age(22);
+
+    User() :id(100), age(22) {}
 };
+
+template<typename T>
+
+T addme(T a, T b) {
+    return a + b;
+}
+
+int getTwo() {
+    return 2;
+}
+
+void intersting() {
+    puts("I am interesting");
+}
+
+void printval(int a) {
+    printf("Integer value is %d\n", a);
+}
+
+void printval(double a) {
+    printf("Double value is %f\n", a);
+}
+
+void printval(int *a) {
+    printf("Pointer value is %p\n", a);
+}
+
+template <typename T>
+
+void func(T t) {
+    console_log2("first func ",t);
+    //console_log(t);
+}
+
+template <typename T,typename... Args>
+void func(T t, Args... args) {
+    console_log2("second func ",t);
+    func(args...);
+}
 
 int main()
 {
-    memoryExample();
+    // memoryExample();
+    // headerFileExample();
+    // pointerAdvance();
+    // nullExample();
+
+    /*EXINT a;
+    console_log("Enter a value: ");
+    cin >> a;
+    EXINT fact = factorial(a);
+    cout << "Factorial : " << fact << endl;
+    ENDMESSAGE;*/
+
+    veriadicExample();
+
+    END;
 }
+
+
+// veriadic operator (...)
+int veriadicExample() {
+
+    string myName = "shyam";
+    func(myName);
+    func(myName, 1, myName, 3.4, myName); // this will be called recursivly
+
+    END;
+}
+
+
+
+// function Recursion
+EXINT factorial(EXINT n) {
+    if (n>1)
+    {
+        return n * factorial(n-1);
+    }
+    else {
+        return n;
+    }
+}
+
+
+
+//nulland nullptr
+int nullExample() {
+
+    printval(NULL);
+    printval(nullptr);
+    END;
+}
+
+
+// function pointer
+int pointerAdvance() {
+
+    cout << "getTwo: " << getTwo() << endl;
+
+    void (*pointsToInteresting)() = intersting;
+    pointsToInteresting();
+    (*pointsToInteresting)(); // derefencing syntax
+    END;
+}
+
+int headerFileExample() {
+    cout << "int addition: " << addme(4, 6) << endl;
+    cout << "float addition: " << addme(4.5f, 6.6f) << endl;
+    END;
+}
+
 
 int memoryExample() {
     
@@ -50,9 +169,12 @@ int memoryExample() {
 
     User* nike = new User();
 
+    cout << "user: id "<< nike->id <<" user: age "<<nike->age <<endl;
 
-    // whenever we use new keyword we need to delete extra memory at the end
+    // whenever we use new keyword we need to delete memory after using
     delete heap_score;
     delete nike;
 
+    cout << "user: id " << nike->id << " user: age " << nike->age << endl;
+    END;
 }
---
+++ adder.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef adder_h
+#define adder_h
+
+int addme(int a, int b) {
+	return a + b;
+}
+
+float addme(float a, float b) {
+	return a + b;
+}
+
+#endif // !adder_h
